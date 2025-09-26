c9b256a16d305c3430aa7adae4801814ae569e4e
igormath <igorxt9@gmail.com>
2025-09-25 21:48:51 -0300

    feat: upgrade app routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import CreateAccount from './components/CreateAccount/CreateAccount';
-import EditUser from './components/EditUser/EditUser';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import CreateAccount from "./components/CreateAccount/CreateAccount";
+import EditUser from "./components/EditUser/EditUser";
+import Homepage from "./components/Homepage/Homepage";
 
 const router = createBrowserRouter([
     {
+        path: "/login",
+        element: <Login />,
+    },
+    {
         path: "/",
-        element: <Login/>
+        element: <Homepage />,
     },
     {
         path: "/home",
-        element: <Home/>
+        element: <Home />,
     },
     {
         path: "/criar-conta",
-        element: <CreateAccount/>
+        element: <CreateAccount />,
     },
     {
         path: "/edit",
-        element: <EditUser/>
-    }
+        element: <EditUser />,
+    },
 ]);
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
+);
